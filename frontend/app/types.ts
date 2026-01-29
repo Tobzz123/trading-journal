e9bfc1f795eba111e96f8e5fbae8de3a38df73fc
Ola Anthony-Egorp <52545507+Tobzz123@users.noreply.github.com>
2026-01-29 10:31:59 -0500

    Merge develop to main (#33)
    * fix: change model to use ActiveRecord enum context, cleanup
    
    * bro what did I even do here?
    
    * remove unnecessary controller

--- frontend/app/types.ts
+++ frontend/app/types.ts
@@ -1,23 +1,28 @@
 export type Trade = {
     id: number;
     ticker: string;
-    trade_type?: TradeType;
+    trade_type: TradeType;
     option_type?: OptionType;
 
     shares?: number;
+    contracts?: number;
+
     entry_price?: number;
     exit_price?: number;
 
-    contracts?: number;
+    
     entry_premium?: number;
     exit_premium?: number;
+
+    entry_datetime?: string;
+    exit_datetime?: string;
+
     strike_price?: number;
     expiration_date?: string;
 
     traded_on?: string;
     notes?: string;
-    entry_datetime?: string;
-    exit_datetime?: string;
+    
 }
 
 export enum TradeType {
