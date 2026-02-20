f70180bc75dcfb15b044c75b8a7af4360de257ef
Tobzz123 <52545507+Tobzz123@users.noreply.github.com>
2026-02-20 11:53:33 -0500

    remove typo

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,5 +1,5 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
-    allow do'
+    allow do
       if Rails.env.development?
         origins "http://localhost:3000", "http://localhost:3001"
       else
